7902ca293dcea87530220f3dbb5bb4416d76fef8
Mohamed Abdelazem <mabdelazemahmed@gmail.com>
2024-09-06 22:05:56 +0300

    fix: enable the cors foramtion for integration between front and back

--- run.py
+++ run.py
@@ -1,11 +1,23 @@
 from fastapi import FastAPI
 from app.routes import router
 from app.db.database import create_tables
+from fastapi.middleware.cors import CORSMiddleware
 
 # Create a new FastAPI instance
 app = FastAPI()
 # Include the router in the app
 app.include_router(router)
+
+# Add CORS middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "*"
+    ],  # Allows all origins. Change this to restrict to specific domains.
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all HTTP methods (GET, POST, PUT, DELETE, etc.)
+    allow_headers=["*"],  # Allows all headers
+)
 
 
 # Initialize the database
