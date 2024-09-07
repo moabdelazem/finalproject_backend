d4dcc4cd9cd01f8ff767389b182ab6b23bd69c30
Mohamed Abdelazem <mabdelazemahmed@gmail.com>
2024-09-07 14:39:33 +0300

    feat: adding the models and schemas

---
+++ app/models.py
@@ -0,0 +1,20 @@
+from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, unique=True, index=True)
+    password = Column(String)
+    is_admin = Column(Boolean, default=False)
+
+
+class Book(Base):
+    __tablename__ = "books"
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String)
+    author = Column(String)
+    is_borrowed = Column(Boolean, default=False)
